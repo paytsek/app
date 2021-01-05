f277d5324cbe66862bf6a2889bdafeb71a5c05b6
Darryl Mangibin <darrylmangibin@gmail.com>
2021-01-05 20:59:52 +0800

    Refactor errorResponse class to have a success status
    Refactor errorResponse class to identify error name

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,7 +1,20 @@
+const ErrorResponse = require('../utils/errorResponse.js');
+
 const errorHandler = (err, req, res, next) => {
-	const errors = { ...err };
-	res.status(res.statusCode);
-	res.json({ ...errors });
+  const error = { ...err };
+
+	if (err.name === 'ValidationError') {
+		const errorFields = Object.keys(error.errors);
+
+		errorFields.forEach((field) => {
+			error.errors[field] = error.errors[field].message;
+		});
+
+		return res.status(400).json({ success: false, ...error });
+	}
+
+	res.status(res.statusCode === 200 ? 500 : res.statusCode);
+	res.json({ success: false, ...error });
 };
 
 module.exports = errorHandler;
