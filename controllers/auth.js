7da16afb2e18331353db75ea7cf6af664d10a1de
Darryl Mangibin <darrylmangibin@gmail.com>
2021-01-07 16:09:09 +0800

    Add user login initial functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const ErrorResponse = require('../utils/errorResponse');
 
 const User = require('../models/User');
 
-// @ROUTE POST /api/v1/auth
+// @ROUTE POST /api/v1/auth/register
 // @Desc Register a user
 // access PUBLIC
 const registerUser = asyncHandler(async (req, res, next) => {
@@ -21,6 +21,42 @@ const registerUser = asyncHandler(async (req, res, next) => {
 	});
 });
 
+// @ROUTE POST /api/v1/auth/login
+// @Desc Login a user
+// access PUBLIC
+const loginUser = asyncHandler(async (req, res, next) => {
+	const { email, password } = req.body;
+
+	if (!email || !password) {
+		res.status(400);
+		return next(
+			new ErrorResponse({ message: 'Please provide an email and password' })
+		);
+	}
+
+	let user = await User.findOne({ email }).select('+password');
+
+	if (!user) {
+		res.status(401);
+		return next(new ErrorResponse({ message: 'Invalid credentials' }));
+	}
+
+	const isMatch = await user.isMatch(password);
+
+	if (!isMatch) {
+		res.status(401);
+		return next(new ErrorResponse({ message: 'Invalid credentials' }));
+	}
+
+	const token = user.generateJwtToken();
+
+	res.status(200).json({
+		success: true,
+		token,
+	});
+});
+
 module.exports = {
 	registerUser,
+	loginUser,
 };
