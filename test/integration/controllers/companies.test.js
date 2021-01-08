3e8ed76b4a809c41a899242d2f456c436dd07b03
Darryl Mangibin <darrylmangibin@gmail.com>
2021-01-08 11:50:48 +0800

    Add test for user and company/company settings

--- test/integration/controllers/companies.test.js
+++ test/integration/controllers/companies.test.js
@@ -1,33 +1,59 @@
 const request = require('supertest');
 const app = require('../../../app');
 const Company = require('../../../models/Company');
+const User = require('../../../models/User');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 
 describe('POST /api/v1/companies/name - createCompany', () => {
 	const url = '/api/v1/companies/name';
 
+	it('should return 401 unauthorized', async () => {
+		const res = await request(app).post(url).send({ name: 'PayTsek' });
+
+		expect(res.status).toBe(401);
+		expect(res.body.success).toBeFalsy();
+		expect(res.body.errors).toMatchObject({
+			message: 'Not authorize to access this route',
+		});
+	});
+
 	it('should return 201 status code and success response if name is invalid', async () => {
-		const { status, body } = await request(app)
+		const token = await global.signIn();
+
+		const res = await request(app)
 			.post(url)
+			.auth(token, { type: 'bearer' })
 			.send({ name: 'PayTsek' });
 
-		expect(status).toBe(201);
-		expect(body.success).toBeTruthy();
-		expect(body.company).toHaveProperty(
+		expect(res.status).toBe(201);
+		expect(res.body.success).toBeTruthy();
+		expect(res.body.company).toHaveProperty(
 			'_id',
-			body.company._id,
+			res.body.company._id,
 			'name',
 			'PayTsek'
 		);
 	});
 
 	it('should generate a slug when saving a company name', async () => {
-		const { body } = await request(app).post(url).send({ name: 'Pay Tsek' });
+		const token = await global.signIn();
+
+		const { body } = await request(app)
+			.post(url)
+			.auth(token, { type: 'bearer' })
+			.send({ name: 'Pay Tsek' });
 
 		expect(body.company.slug).toMatch(/pay-tsek/);
 	});
 
 	it('should return 400 status code and error response if name is empty', async () => {
-		const { status, body } = await request(app).post(url).send({ name: '' });
+		const token = await global.signIn();
+
+		const { status, body } = await request(app)
+			.post(url)
+			.auth(token, { type: 'bearer' })
+			.send({ name: '' });
 
 		expect(status).toBe(400);
 		expect(body.success).toBeFalsy();
@@ -35,9 +61,12 @@ describe('POST /api/v1/companies/name - createCompany', () => {
 	});
 
 	it('should return 400 status code and error response if name already exist', async () => {
-		await Company.create({ name: 'PayTsek' });
+		const token = await global.signIn();
+
+		await Company.create({ name: 'PayTsek', user: mongoose.Types.ObjectId() });
 		const { status, body } = await request(app)
 			.post(url)
+			.auth(token, { type: 'bearer' })
 			.send({ name: 'PayTsek' });
 
 		expect(status).toBe(400);
@@ -50,7 +79,10 @@ describe('GET api/v1/companies', () => {
 	const url = '/api/v1/companies';
 
 	it('should get all companies and 200 status code', async () => {
-		await Company.create([{ name: 'PayTsek' }, { name: 'Fullsuite' }]);
+		await Company.create([
+			{ name: 'PayTsek', user: mongoose.Types.ObjectId() },
+			{ name: 'Fullsuite', user: mongoose.Types.ObjectId() },
+		]);
 
 		const { status, body } = await request(app).get(url);
 
@@ -70,7 +102,10 @@ describe('GET /api/v1/companies/:id - single company', () => {
 	const url = '/api/v1/companies';
 
 	it('should return 200 status and get a company by id', async () => {
-		const company = await Company.create({ name: 'PayTsek' });
+		const company = await Company.create({
+			name: 'PayTsek',
+			user: mongoose.Types.ObjectId(),
+		});
 
 		const { status, body } = await request(app).get(`${url}/${company._id}`);
 
@@ -82,6 +117,95 @@ describe('GET /api/v1/companies/:id - single company', () => {
 				slug: 'paytsek',
 				_id: company._id.toString(),
 			})
+		);
+	});
+});
+
+describe('POST /api/v1/companies/:id/settings', () => {
+	const url = '/api/v1/companies';
+
+	it('should return 400 Not found and error response if company is invalid or not found', async () => {
+		const token = await global.signIn();
+
+		const id = mongoose.Types.ObjectId();
+
+		const res = await request(app)
+			.post(`${url}/${id}/settings`)
+			.auth(token, { type: 'bearer' });
+
+		expect(res.status).toBe(404);
+		expect(res.body.success).toBeFalsy();
+		expect(res.body.errors).toMatchObject({
+			message: 'No company is selected',
+		});
+	});
+
+	it('should return 401 if not logged in', async () => {
+		const company = await Company.create({
+			name: 'PayTsek',
+			user: mongoose.Types.ObjectId(),
+		});
+
+		const res = await request(app)
+			.post(`${url}/${company._id}/settings`)
+			.send({});
+
+		expect(res.status).toBe(401);
+		expect(res.body.success).toBeFalsy();
+		expect(res.body.errors).toMatchObject({
+			message: 'Not authorize to access this route',
+		});
+	});
+
+	it('should return 401 if logged in user is not equal to company owner', async () => {
+		const token = await global.signIn();
+
+		const user = await User.create({
+			username: 'john doe',
+			email: 'johndoe@gmail.com',
+			password: '123456',
+		});
+
+		const company = await Company.create({
+			name: 'test company',
+			user: user._id,
+		});
+
+		const res = await request(app)
+			.post(`${url}/${company._id}/settings`)
+			.auth(token, { type: 'bearer' });
+
+		expect(res.status).toBe(401);
+		expect(res.body.success).toBeFalsy();
+		expect(res.body.errors).toMatchObject({
+			message: 'User is not authorized',
+		});
+	});
+
+	it('should return 201 status code and success response if fields are valid', async () => {
+		const token = await global.signIn();
+
+		let res = await request(app)
+			.post('/api/v1/companies/name')
+			.auth(token, { type: 'bearer' })
+			.send({ name: 'PayTsek' });
+
+		const { company } = res.body;
+
+		res = await request(app)
+			.post(`${url}/${company._id}/settings`)
+			.auth(token, { type: 'bearer' })
+			.send({
+				secondPayout: 30,
+				firstPayout: 1,
+				secondCutOff: 20,
+				firstCutOff: 5,
+			});
+
+		expect(res.status).toBe(201);
+		expect(res.body.success).toBeTruthy();
+		expect(Object.keys(res.body.companySettings)).toEqual(
+			expect.arrayContaining(['_id'])
 		);
 	});
 });
--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,7 @@
 const { MongoMemoryServer } = require('mongodb-memory-server');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
 let mongo;
 
@@ -27,3 +29,23 @@ afterAll(async () => {
 	await mongo.stop();
 	await mongoose.connection.close();
 });
+
+global.signIn = async () => {
+  const user = await User.create({
+    username: 'John Doe',
+    email: 'john@example.com',
+    password: '123456'
+  })
+
+	const payload = {
+    id: user.id,
+    email: user.email,
+    username: user.username
+	};
+
+	const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+		expiresIn: 3600,
+  });
+  
+	return token;
+};
