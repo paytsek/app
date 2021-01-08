df29141d710f2c66b3ddeb19eacb33db1a72e203
Darryl Mangibin <darrylmangibin@gmail.com>
2021-01-08 14:01:25 +0800

    Add test for users

--- middleware/auth.js
+++ middleware/auth.js
@@ -16,21 +16,26 @@ const auth = asyncHandler(async (req, res, next) => {
 
 	if (!token) {
 		res.status(401);
-		return next(
-			new ErrorResponse({ message: 'Not authorize to access this route' })
-		);
-  }
-  
-  try {
-    const decoded = await jwt.verify(token, process.env.JWT_SECRET_KEY);
+		return next(new ErrorResponse({ message: 'No token, access denied' }));
+	}
 
-    req.user = await User.findById(decoded.id)
+	try {
+		const decoded = await jwt.verify(token, process.env.JWT_SECRET_KEY);
 
-    next()
-  } catch(error) {
-    res.status(401)
-    next(new ErrorResponse({ message: 'Not authorize to access this route' }))
-  }
+		const user = await User.findById(decoded.id);
+
+		if (!user) {
+			res.status(401);
+			return next(new ErrorResponse({ message: 'No user, access denied' }));
+		}
+
+		req.user = user;
+
+		next();
+	} catch (error) {
+		res.status(401);
+		next(new ErrorResponse({ message: 'Not authorize to access this route' }));
+	}
 });
 
 module.exports = auth;
