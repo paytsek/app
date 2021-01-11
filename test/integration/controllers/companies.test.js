72372e270afaf2cd2819e221335338937ae8a49b
Darryl Mangibin <darrylmangibin@gmail.com>
2021-01-11 13:31:39 +0800

    Add test for deleting a company

--- test/integration/controllers/companies.test.js
+++ test/integration/controllers/companies.test.js
@@ -1,9 +1,11 @@
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 const request = require('supertest');
+
 const app = require('../../../app');
 const Company = require('../../../models/Company');
 const User = require('../../../models/User');
-const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
+const CompanySetting = require('../../../models/CompanySetting');
 
 describe('POST /api/v1/companies/name - createCompany', () => {
 	const url = '/api/v1/companies/name';
@@ -177,6 +179,94 @@ describe('PUT /api/v1/companies/name/:id - updateCompanyName', () => {
 				expect.objectContaining({ _id: company._id }),
 				expect.objectContaining({ user: user._id })
 			);
+		});
+	});
+});
+
+describe('DELETE /api/v1/companies/name/:id - deleteCompany', () => {
+	const url = '/api/v1/companies/name';
+
+	describe('Error Response', () => {
+		it('should return error response if not logged in', async () => {
+			const res = await request(app).delete(
+				`${url}/${mongoose.Types.ObjectId()}`
+			);
+
+			expect(res.status).toBe(401);
+			expect(res.body.success).toBeFalsy();
+			expect(res.body.errors).toMatchObject({
+				message: 'No token, access denied',
+			});
+		});
+
+		it('should return error response if id params is invalid', async () => {
+			const id = mongoose.Types.ObjectId();
+			const token = await global.signIn();
+
+			const res = await request(app)
+				.delete(`${url}/${id}`)
+				.auth(token, { type: 'bearer' });
+
+			expect(res.status).toBe(404);
+			expect(res.body.success).toBeFalsy();
+			expect(res.body.errors).toMatchObject({
+				message: `Resource with an id of ${id} not found`,
+			});
+		});
+
+		it('should return error response if logged user not own the company', async () => {
+			const token = await global.signIn();
+			const user = await User.create({
+				username: 'rodrigocarlos',
+				email: 'rodrigo@gmail.com',
+				password: '123456',
+				firstName: 'Rodrigo',
+				lastName: 'Carlos',
+			});
+			const company = await Company.create({
+				name: 'Sample company',
+				user: user._id,
+			});
+
+			const res = await request(app)
+				.delete(`${url}/${company._id}`)
+				.auth(token, { type: 'bearer' });
+
+			expect(res.status).toBe(401);
+			expect(res.body.success).toBeFalsy();
+			expect(res.body.errors).toMatchObject({
+				message: 'Not authorized to access this route',
+			});
+		});
+	});
+
+	describe('Success Response', () => {
+		it('should return success response if values are valid', async () => {
+			const token = await global.signIn();
+			const user = jwt.verify(token, process.env.JWT_SECRET_KEY);
+			const company = await Company.create({ name: 'PayTsek', user: user._id });
+			const companySettings = await CompanySetting.create({
+				company: company._id,
+				frequency: 'monthly',
+				firstCutOff: 1,
+				firstPayout: 5,
+			});
+
+			expect(await CompanySetting.countDocuments()).toBe(1);
+
+			const res = await request(app)
+				.delete(`${url}/${company._id}`)
+				.auth(token, { type: 'bearer' });
+
+			expect(res.status).toBe(200);
+			expect(res.body.success).toBeTruthy();
+			expect(Object.keys(res.body.company).length).toEqual(0);
+			expect(res.body).toEqual(
+				expect.objectContaining({
+					message: `PayTsek - ID:${company._id} successfully deleted`,
+				})
+			);
+			expect(await CompanySetting.countDocuments()).toEqual(0);
 		});
 	});
 });
