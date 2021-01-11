f8262ba1dccce8bb6f1c8d8a866736878d018b8a
Darryl Mangibin <darrylmangibin@gmail.com>
2021-01-11 17:41:13 +0800

    Add test for deleting company settings

--- test/integration/controllers/companies.test.js
+++ test/integration/controllers/companies.test.js
@@ -817,3 +817,105 @@ describe('PUT /api/v1/companies/:id/settings/:companySettingsId - updateCompanyS
 		});
 	});
 });
+
+describe('DELETE /api/v1/companies/:id/settings/:companySettingsId - deleteCompanySettings', () => {
+	const url = '/api/v1/companies';
+
+	describe('Error Response', () => {
+		it('should return error response if not logged in', async () => {
+			const res = await request(app).put(
+				`${url}/${mongoose.Types.ObjectId()}/settings/${mongoose.Types.ObjectId()}`
+			);
+
+			expect(res.status).toBe(401);
+			expect(res.body.success).toBeFalsy();
+			expect(res.body.errors).toMatchObject({
+				message: 'No token, access denied',
+			});
+		});
+
+		it('should return error if company not own by the logged in user', async () => {
+			const user = await User.create({
+				username: 'rodrigocarlos',
+				email: 'rodrigo@gmail.com',
+				password: '123456',
+				firstName: 'Rodrigo',
+				lastName: 'Carlos',
+			});
+			const company = await Company.create({ name: 'PayTsek', user: user._id });
+			const token = await global.signIn();
+
+			const res = await request(app)
+				.put(`${url}/${company._id}/settings/${mongoose.Types.ObjectId()}`)
+				.auth(token, { type: 'bearer' });
+
+			expect(res.status).toBe(401);
+			expect(res.body.success).toBeFalsy();
+			expect(res.body.errors).toMatchObject({
+				message: 'Not authorized to access this route',
+			});
+		});
+
+		it('should return error if company id params is invalid', async () => {
+			const token = await global.signIn();
+			const id = mongoose.Types.ObjectId();
+
+			const res = await request(app)
+				.put(`${url}/${id}/settings/${mongoose.Types.ObjectId()}`)
+				.auth(token, { type: 'bearer' });
+
+			expect(res.status).toBe(401);
+			expect(res.body.success).toBeFalsy();
+			expect(res.body.errors).toMatchObject({
+				message: 'Invalid Company id',
+			});
+		});
+
+		it('should return error if company does not have settings created', async () => {
+			const token = await global.signIn();
+			const loggedInUser = jwt.verify(token, process.env.JWT_SECRET_KEY);
+			const companySettingsId = mongoose.Types.ObjectId();
+			const company = await Company.create({
+				name: 'PayTsek',
+				user: loggedInUser._id,
+			});
+
+			const res = await request(app)
+				.put(`${url}/${company._id}/settings/${companySettingsId}`)
+				.auth(token, { type: 'bearer' });
+
+			expect(res.status).toBe(404);
+			expect(res.body.success).toBeFalsy();
+			expect(res.body.errors).toMatchObject({
+				message: `Resource with an id of ${companySettingsId} not found`,
+			});
+		});
+	});
+
+	describe('Success Response', () => {
+		it('should return success response if logged in user, comany and settings are valid', async () => {
+			const token = await global.signIn();
+			const user = jwt.verify(token, process.env.JWT_SECRET_KEY);
+			const company = await Company.create({ name: 'PayTsek', user: user._id });
+			const companySettings = await CompanySetting.create({
+				frequency: 'monthly',
+				firstPayout: 5,
+				firstCutOff: 1,
+				company: company._id,
+			});
+
+			const res = await request(app)
+				.delete(`${url}/${company._id}/settings/${companySettings._id}`)
+				.auth(token, { type: 'bearer' });
+
+			expect(res.status).toBe(200);
+			expect(res.body.success).toBeTruthy();
+			expect(Object.keys(res.body.companySettings).length).toEqual(0);
+			expect(res.body).toEqual(
+				expect.objectContaining({
+					message: `Company Settings - ID:${companySettings._id} successfully deleted`,
+				})
+			);
+		});
+	});
+});
