19e3db2a31ae4be0c97bb698b13c0796ef32a2ac
Daniel <daniel@fullsuite.ph>
2021-01-12 13:42:49 +0800

    Added Register Page mark up

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import { Fragment } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
+import Register from './pages/Register'
+
 const App = () => {
 	return (
 		<Fragment>
 			<CssBaseline />
+			<Register />
       {/* DITO YUNG CODE NA IBA */}
 		</Fragment>
 	);
