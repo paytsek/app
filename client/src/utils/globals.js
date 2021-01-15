7d9eec4782c5bccccd6b060b24088ace43106fc5
Darryl Mangibin <darrylmangibin@gmail.com>
2021-01-15 14:33:05 +0800

    Add accounting journals as globals

---
+++ client/src/utils/globals.js
@@ -0,0 +1,14 @@
+export const ACCOUNTING_JOURNAL_ENTRIES = [
+	{ name: 'Wages and Salaries', value: 'wagesAndSalaries' },
+	{ name: 'Wages Payable', value: 'wagesPayable' },
+	{ name: 'SSS Payable', value: 'ssspayable' },
+	{ name: 'HDMF Payable', value: 'hdmfPayable' },
+	{ name: 'PHIC Payable', value: 'phicPayable' },
+	{ name: 'Thirtheenth Month Pay', value: 'thirtheenthMonthPay' },
+	{ name: 'Employee Benefits', value: 'employeeBenefits' },
+	{ name: 'Advances To Employee', value: 'advancesToEmployee' },
+	{
+		name: 'Withholding Tax On Compensation Payable',
+		value: 'withholdingTaxOnCompensationPayable',
+	},
+];
