28890d53cda3bfaf80557a7ce5bfc6dc2ed4463d
Darryl Mangibin <darrylmangibin@gmail.com>
2021-01-19 11:14:37 +0800

    Add protected routes or private routes

---
+++ client/src/components/routes/PrivateRoute/index.js
@@ -0,0 +1,19 @@
+import { Route, Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+	const { auth, loading } = useSelector((state) => state.authUser);
+	return (
+		<Route
+			{...rest}
+			render={(props) => {
+				if (!auth && !loading) {
+					return <Redirect to="/login" />;
+				}
+				return <Component {...props} />;
+			}}
+		/>
+	);
+};
+
+export default PrivateRoute;
