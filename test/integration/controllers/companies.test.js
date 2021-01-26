3b4dea16418c7a345f37f2002fead9be31690854
Darryl Mangibin <darrylmangibin@gmail.com>
2021-01-26 11:11:10 +0800

    Update companies test to test admin deleting a company

--- test/integration/controllers/companies.test.js
+++ test/integration/controllers/companies.test.js
@@ -252,6 +252,40 @@ describe('DELETE /api/v1/companies/name/:id - deleteCompany', () => {
       );
       expect(await CompanySetting.countDocuments()).toEqual(0);
     });
+
+    it('should return success response logged in is an admin', async () => {
+      const token = await global.signInAdmin();
+      const user = await User.create({
+        username: 'rodrigo',
+        email: 'rodrigo@gmail.com',
+        password: '123456',
+        firstName: 'Rodrigo',
+        lastName: 'Carlos',
+      });
+      const company = await Company.create({ name: 'PayTsek', user: user._id });
+      await CompanySetting.create({
+        company: company._id,
+        frequency: 'monthly',
+        firstCutOff: 1,
+        firstPayout: 5,
+      });
+
+      expect(await CompanySetting.countDocuments()).toBe(1);
+
+      const res = await request(app)
+        .delete(`${url}/${company._id}`)
+        .auth(token, { type: 'bearer' });
+
+      expect(res.status).toBe(200);
+      expect(res.body.success).toBeTruthy();
+      expect(Object.keys(res.body.company).length).toEqual(0);
+      expect(res.body).toEqual(
+        expect.objectContaining({
+          message: `PayTsek - ID:${company._id} successfully deleted`,
+        }),
+      );
+      expect(await CompanySetting.countDocuments()).toEqual(0);
+    });
   });
 });
 
