6abdcc6b7de4974228b0f39f52ce733e933e390f
Darryl Mangibin <darrylmangibin@gmail.com>
2021-01-28 22:30:42 +0800

    Add setCompanySlug utils to set slug to the headers

---
+++ client/src/utils/setCompanySlug.js
@@ -0,0 +1,11 @@
+import axios from '../axios';
+
+const setCompanySlug = slug => {
+  if (slug) {
+    axios.defaults.headers.common['x-company-slug'] = slug;
+  } else {
+    delete axios.defaults.headers.common['x-company-slug'];
+  }
+};
+
+export default setCompanySlug;
