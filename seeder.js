97f73e956d82b2d9e557c21b1677f0dee27b4e24
Darryl Mangibin <darrylmangibin@gmail.com>
2021-01-28 11:49:23 +0800

    Add companySettings data to the seeder

--- seeder.js
+++ seeder.js
@@ -7,6 +7,7 @@ dotenv.config({ path: './config/config.env' });
 
 const User = require('./models/User');
 const Company = require('./models/Company');
+const CompanySetting = require('./models/CompanySetting');
 
 const db = process.env.MONGO_URI_DEV;
 
@@ -20,11 +21,13 @@ mongoose.connect(db, {
 // Read JSON
 const users = JSON.parse(fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8'));
 const companies = JSON.parse(fs.readFileSync(`${__dirname}/_data/companies.json`, 'utf-8'));
+const companySettings = JSON.parse(fs.readFileSync(`${__dirname}/_data/companySettings.json`, 'utf-8'));
 
 const importData = async () => {
   try {
     await User.create(users);
     await Company.create(companies);
+    await CompanySetting.create(companySettings);
 
     console.log('Data imported...'.green.inverse);
     process.exit(1);
@@ -38,6 +41,7 @@ const deleteData = async () => {
   try {
     await User.deleteMany({});
     await Company.deleteMany({});
+    await CompanySetting.deleteMany({});
 
     console.log('Data deleted...'.red.inverse);
     process.exit(1);
