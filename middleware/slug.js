b642e4e7515e89804dcee16afdb704a3c0784f77
Darryl Mangibin <darrylmangibin@gmail.com>
2021-01-28 22:29:52 +0800

    Add slug middleware

--- middleware/slug.js
+++ middleware/slug.js
@@ -4,16 +4,25 @@ const ErrorResponse = require('../utils/errorResponse');
 const Company = require('../models/Company');
 
 const slug = asyncHandler(async (req, res, next) => {
-  const company = await Company.findOne({ slug: req.company.slug });
+  let companySlug;
+
+  if (req.headers['x-company-slug']) {
+    companySlug = req.headers['x-company-slug'];
+  }
+
+  if (!companySlug) {
+    res.status(401);
+    return next(new ErrorResponse({ message: 'No slug, access denied' }));
+  }
+
+  const company = await Company.findOne({ slug: companySlug });
 
   if (!company) {
-    res.status(403);
-    return next(
-      new ErrorResponse({
-        message: 'No Company, access denied',
-      }),
-    );
+    res.status(401);
+    return next(new ErrorResponse({ message: 'No Company, access denied' }));
   }
+
+  req.company = company;
 
   return next();
 });
