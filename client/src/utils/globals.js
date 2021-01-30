aaec7575007561ac3de14af164369c025bb8a1c4
Darryl Mangibin <darrylmangibin@gmail.com>
2021-01-30 15:01:54 +0800

    Fix typos in globals for ACCOUNTING_JOURNAL_ENTRIES

--- client/src/utils/globals.js
+++ client/src/utils/globals.js
@@ -1,12 +1,12 @@
 export const ACCOUNTING_JOURNAL_ENTRIES = [
   { name: 'Wages and Salaries', value: 'wagesAndSalaries' },
   { name: 'Wages Payable', value: 'wagesPayable' },
-  { name: 'SSS Payable', value: 'ssspayable' },
+  { name: 'SSS Payable', value: 'sssPayable' },
   { name: 'HDMF Payable', value: 'hdmfPayable' },
   { name: 'PHIC Payable', value: 'phicPayable' },
   { name: 'Thirteenth Month Pay', value: 'thirteenthMonthPay' },
   { name: 'Employee Benefits', value: 'employeeBenefits' },
-  { name: 'Advances To Employee', value: 'advancesToEmployee' },
+  { name: 'Advances To Employees', value: 'advancesToEmployees' },
   {
     name: 'Withholding Tax On Compensation Payable',
     value: 'withholdingTaxOnCompensationPayable',
