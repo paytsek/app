92adf606419c5ea3b9da49ac7e7125d8096fa0ce
Darryl Mangibin <darrylmangibin@gmail.com>
2021-02-01 11:05:16 +0800

    Add static background image for login page

--- client/src/pages/LoginPage/index.jsx
+++ client/src/pages/LoginPage/index.jsx
@@ -34,7 +34,7 @@ const LoginPage = ({ history }) => {
   return (
     <Grid container component="main" className={classes.root}>
       <CssBaseline />
-      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={false} sm={4} md={7} />
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
         <div className={classes.paper}>
           <Avatar className={classes.avatar}>
