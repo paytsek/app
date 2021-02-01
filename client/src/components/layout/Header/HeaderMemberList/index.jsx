bf2b7f8e121fd2d605cde1496f154100e5e29342
Darryl Mangibin <darrylmangibin@gmail.com>
2021-02-01 15:56:56 +0800

    Move company list member link to user tool bar

--- client/src/components/layout/Header/HeaderMemberList/index.jsx
+++ client/src/components/layout/Header/HeaderMemberList/index.jsx
@@ -3,7 +3,6 @@ import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
 import {
-  Business as BusinessIcon,
   BusinessCenter as BusinessCenterIcon,
   Dashboard as DashboardIcon,
 } from '@material-ui/icons';
@@ -19,14 +18,6 @@ const HeaderAdminList = () => {
             <DashboardIcon />
           </ListItemIcon>
           <ListItemText primary="Dashboard" />
-        </ListItem>
-      </Link>
-      <Link to={`/${slug}/companies`}>
-        <ListItem button>
-          <ListItemIcon>
-            <BusinessIcon />
-          </ListItemIcon>
-          <ListItemText primary="Companies" />
         </ListItem>
       </Link>
       <Link to={`/${slug}/company-settings`}>
