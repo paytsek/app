e47e42681966b156477e8fc0e8921b546c7e66ee
Darryl Mangibin <darrylmangibin@gmail.com>
2021-02-02 14:42:35 +0800

    Add baseUrl

--- client/src/axios/index.js
+++ client/src/axios/index.js
@@ -1,5 +1,12 @@
 import axios from 'axios';
 
+const baseURL =
+	(process.env.NODE_ENV === 'local' && 'http://localhost:5000/api/v1') ||
+	(process.env.NODE_ENV === 'development' &&
+		'https://staging-paytsek.herokuapp.com/api/v1') ||
+	(process.env.NODE_ENV === 'production' &&
+		'https://staging-paytsek.herokuapp.com/api/v1');
+
 export default axios.create({
-  baseURL: 'http://localhost:5000/api/v1',
+	baseURL,
 });
