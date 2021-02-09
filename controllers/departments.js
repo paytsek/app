8c49f0dad9a3b0bd05b8cc1fa20492995bc23b96
Darryl Mangibin <darrylmangibin@gmail.com>
2021-02-09 16:38:07 +0800

    Add method to delete a department

--- controllers/departments.js
+++ controllers/departments.js
@@ -4,6 +4,9 @@ const User = require('../models/User');
 const asyncHandler = require('../middleware/asyncHandler');
 const ErrorResponse = require('../utils/errorResponse');
 
+// @ROUTE GET /api/v1/departments
+// @Desc Get all departments of the company settings
+// access PRIVATE - Logged in user
 const getDepartments = asyncHandler(async (req, res, next) => {
   const company = await Company.findById(req.company._id);
   const user = await User.findById(req.user._id);
@@ -18,6 +21,9 @@ const getDepartments = asyncHandler(async (req, res, next) => {
   return res.status(200).json({ success: true, departments });
 });
 
+// @ROUTE PUT /api/v1/departments/:id
+// @Desc Update a department by id
+// access PRIVATE - Logged in user
 const updateDepartment = asyncHandler(async (req, res, next) => {
   const company = await Company.findById(req.company._id).populate({
     path: 'companySettings',
@@ -65,7 +71,51 @@ const updateDepartment = asyncHandler(async (req, res, next) => {
     .json({ success: true, department, message: 'Department successfully updated' });
 });
 
+// @ROUTE DELETE /api/v1/departments/:id
+// @Desc Delete a department by id
+// access PRIVATE - Logged in user
+const deleteDepartment = asyncHandler(async (req, res, next) => {
+  const company = await Company.findById(req.company._id).populate({
+    path: 'companySettings',
+    select: 'departments',
+    populate: {
+      path: 'departments',
+    },
+  });
+  const user = await User.findById(req.user._id);
+
+  if (!company || !user || company.user.toString() !== user._id.toString()) {
+    res.status(401);
+    return next(new ErrorResponse({ message: 'Not authorized, access denied' }));
+  }
+
+  if (!company.companySettings) {
+    res.status(401);
+    return next(new ErrorResponse({ message: 'No Company settings, access denied' }));
+  }
+
+  const departmentExistInCompany = company.companySettings.departments.find(
+    (department) => department._id.toString() === req.params.id,
+  );
+
+  if (!departmentExistInCompany) {
+    res.status(404);
+    return next(
+      new ErrorResponse({ message: `Resource with an id of ${req.params.id} not found` }),
+    );
+  }
+
+  await Department.findByIdAndDelete(req.params.id);
+
+  return res.status(200).json({
+    success: true,
+    department: {},
+    message: `Department - ID:${req.params.id} successfully deleted`,
+  });
+});
+
 module.exports = {
   getDepartments,
   updateDepartment,
+  deleteDepartment,
 };
