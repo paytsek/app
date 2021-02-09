33228684a4383f1152977368136900ab357c47a1
Darryl Mangibin <darrylmangibin@gmail.com>
2021-02-09 12:49:12 +0800

    Update company settings model and add department model

--- models/Department.js
+++ models/Department.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const DepartmentSchema = new mongoose.Schema(
   {
@@ -11,10 +12,18 @@ const DepartmentSchema = new mongoose.Schema(
       ref: 'Company',
       required: [true, 'Company must exist'],
     },
+    companySettings: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'CompanySetting',
+      required: [true, 'Company Setting must exist'],
+    },
   },
   {
     timestamps: true,
+    autoCreate: true,
   },
 );
+
+DepartmentSchema.plugin(uniqueValidator, { type: 'mongoose-unique-validator' });
 
 module.exports = mongoose.model('Department', DepartmentSchema);
