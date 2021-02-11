5f59a99a59d0cc3c913dc9f26fb76bebe72c0a4f
Darryl Mangibin <darrylmangibin@gmail.com>
2021-02-11 12:56:00 +0800

    Add Compensation model

---
+++ models/Compensation.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const CompensationSchema = new mongoose.Schema({
+  basicPay: {
+    type: Number,
+    required: [true, 'Basic Pay is required'],
+  },
+  dailyRate: {
+    type: Number,
+  },
+  hourlyRate: {
+    type: Number,
+  },
+  effectivityDate: {
+    type: Date,
+    default: Date.now,
+  },
+  employee: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Employee',
+    required: [true, 'Employee must exist'],
+  },
+  company: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Company',
+    required: [true, 'Company must exist'],
+  },
+});
+
+module.exports = mongoose.model('Compensation', CompensationSchema);
