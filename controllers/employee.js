73f5963b226dea3b674387fb97d00bf78ba0c698
Darryl Mangibin <darrylmangibin@gmail.com>
2021-02-15 15:38:32 +0800

    Add method to fetch single employee of the company

--- controllers/employee.js
+++ controllers/employee.js
@@ -33,7 +33,30 @@ const getEmployees = asyncHandler(async (req, res, next) => {
 // @Desc Get single employee for a specific company
 // access PRIVATE - Logged in user
 const getEmployee = asyncHandler(async (req, res, next) => {
-  res.send('Get employee');
+  const company = await Company.findById(req.company._id);
+  const user = await User.findById(req.user._id);
+
+  if (
+    user.role !== 'admin' &&
+    (!company || !user || company.user.toString() !== user._id.toString())
+  ) {
+    res.status(401);
+    return next(new ErrorResponse({ message: 'Not authorized, access denied' }));
+  }
+
+  const employee = await Employee.findOne({ _id: req.params.id, company: company._id }).populate(
+    'department',
+    'name',
+  );
+
+  if (!employee) {
+    res.status(404);
+    return next(
+      new ErrorResponse({ message: `Resource with an id of ${req.params.id} not found` }),
+    );
+  }
+
+  return res.status(200).json({ success: true, employee });
 });
 
 // @ROUTE POST /api/v1/employees
