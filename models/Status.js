65225eec8c8c1b886f221acf61d0faf0833c4ca6
Darryl Mangibin <darrylmangibin@gmail.com>
2021-02-15 14:25:29 +0800

    Add method for creating an employee

--- models/Status.js
+++ models/Status.js
@@ -3,35 +3,47 @@ const moment = require('moment');
 
 const Employee = require('./Employee');
 
-const StatusSchema = new mongoose.Schema({
-  active: {
-    type: Boolean,
-    default: false,
+const StatusSchema = new mongoose.Schema(
+  {
+    active: {
+      type: Boolean,
+      default: true,
+    },
+    effectivityDate: {
+      type: Date,
+      default: Date.now,
+    },
+    employee: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Employee',
+      required: [true, 'Employee must exist'],
+    },
+    company: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Company',
+      required: [true, 'Company must exist'],
+    },
   },
-  effectivityDate: {
-    type: Date,
-    default: Date.now,
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+    autoCreate: true,
   },
-  employee: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Employee',
-    required: [true, 'Employee must exist'],
-  },
-  company: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Company',
-    required: [true, 'Company must exist'],
-  },
-});
+);
 
 StatusSchema.post('save', async (doc, next) => {
   const statuses = await mongoose.model('Status').find({ employee: doc.employee });
 
-  const status = statuses.reduce(
-    (acc, val) => moment().valueOf(acc.effectivityDate) > moment().valueOf(val.effectivityDate),
-  );
+  if (statuses.length <= 0) {
+    await Employee.findByIdAndUpdate(doc.employee, { status: doc });
+  } else {
+    const status = statuses.reduce(
+      (acc, val) => moment().valueOf(acc.effectivityDate) > moment().valueOf(val.effectivityDate),
+    );
 
-  await Employee.findByIdAndUpdate(doc.employee, { status });
+    await Employee.findByIdAndUpdate(doc.employee, { status });
+  }
 
   next();
 });
