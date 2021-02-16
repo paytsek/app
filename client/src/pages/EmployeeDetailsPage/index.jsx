6d9efa2293b14b09a90090b5df9cb493f7d4f4d7
Darryl Mangibin <darrylmangibin@gmail.com>
2021-02-16 11:35:00 +0800

    Add EmployeeDetailsPages

--- client/src/pages/EmployeeDetailsPage/index.jsx
+++ client/src/pages/EmployeeDetailsPage/index.jsx
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Container, Breadcrumbs, Paper, Typography, Button, Grid } from '@material-ui/core';
+import {
+  Container,
+  Breadcrumbs,
+  Paper,
+  Typography,
+  Button,
+  Grid,
+  List,
+  ListItem,
+  ListItemText,
+} from '@material-ui/core';
 import { Edit } from '@material-ui/icons';
+import moment from 'moment';
 
+import MuiSkeleton from '../../components/MuiSkeleton';
+
+import { getEmployeeDetails } from '../../redux/actions/employeesActions';
 import useStyles from './styles';
+import { EMPLOYEE_DETAILS_RESET } from '../../redux/types/employeeTypes';
 
 const EmployeeDetailsPage = ({ match }) => {
   const { id, slug } = match.params;
+  const dispatch = useDispatch();
+
+  const { employee, loading } = useSelector((state) => state.employeeDetails);
+
+  console.log(employee);
 
   const { active, paper, title, actions, details } = useStyles();
+
+  useEffect(() => {
+    dispatch(getEmployeeDetails(id));
+
+    return () => {
+      dispatch({ type: EMPLOYEE_DETAILS_RESET });
+    };
+  }, []);
 
   return (
     <Container>
@@ -23,21 +52,203 @@ const EmployeeDetailsPage = ({ match }) => {
       </Breadcrumbs>
       <Paper className={paper}>
         <Typography variant="h5" className={title} gutterBottom>
-          View Employee Information
+          View Employee
         </Typography>
         <div className={actions}>
           <Button variant="contained" color="primary" startIcon={<Edit />} size="small">
             Edit
           </Button>
         </div>
-        <Grid container spacing={1}>
-          <Grid item xs={12} md={6}>
-            <div className={details}>
-              <Typography variant="subtitle2">Employee No.</Typography>
-              <Typography variant="subtitle1">123</Typography>
-            </div>
-          </Grid>
-        </Grid>
+        {loading ? (
+          <MuiSkeleton />
+        ) : (
+          <>
+            <Grid container spacing={1}>
+              <Grid item xs={12} md={6}>
+                <Typography variant="h5" gutterBottom>
+                  Employee Information
+                </Typography>
+                <div className={details}>
+                  <Typography variant="subtitle2">Email</Typography>
+                  <Typography variant="subtitle1">{employee.email || '--'}</Typography>
+                </div>
+                <div className={details}>
+                  <Typography variant="subtitle2">Status</Typography>
+                  <Typography variant="subtitle1">
+                    {employee.status.active
+                      ? `Active since ${moment(employee.status.effectivityDate).format(
+                        'MMM DD, YYYY',
+                      )}`
+                      : `Inactive since${moment(employee.status.effectivityDate).format(
+                        'MMM DD, YYYY',
+                      )}`}
+                  </Typography>
+                </div>
+                <div className={details}>
+                  <Typography variant="subtitle2">Employee Number</Typography>
+                  <Typography variant="subtitle1">{employee.employeeNumber || '--'}</Typography>
+                </div>
+                <div className={details}>
+                  <Typography variant="subtitle2">First Name</Typography>
+                  <Typography variant="subtitle1">{employee.firstName}</Typography>
+                </div>
+                <div className={details}>
+                  <Typography variant="subtitle2">Last Name</Typography>
+                  <Typography variant="subtitle1">{employee.lastName}</Typography>
+                </div>
+                <div className={details}>
+                  <Typography variant="subtitle2">Department</Typography>
+                  <Typography variant="subtitle1">{employee.department.name || '--'}</Typography>
+                </div>
+                <div className={details}>
+                  <Typography variant="subtitle2">Position</Typography>
+                  <Typography variant="subtitle1">{employee.position || ''}</Typography>
+                </div>
+                <div className={details}>
+                  <Typography variant="subtitle2">Hire Date</Typography>
+                  <Typography variant="subtitle1">
+                    {(employee.hireDate && moment(employee.hireDate).format('MMM DD, YYYY')) ||
+                      '--'}
+                  </Typography>
+                </div>
+                {employee.resignationDate && (
+                  <div className={details}>
+                    <Typography variant="subtitle2">Resignation Date</Typography>
+                    <Typography variant="subtitle1">
+                      {(employee.hireDate &&
+                        moment(employee.resignationDate).format('MMM DD, YYYY')) ||
+                        '--'}
+                    </Typography>
+                  </div>
+                )}
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <Typography variant="h5" gutterBottom>
+                  Personal Information
+                </Typography>
+                <div className={details}>
+                  <Typography variant="subtitle2">Gender</Typography>
+                  <Typography variant="subtitle1">{employee.gender || '--'}</Typography>
+                </div>
+                <div className={details}>
+                  <Typography variant="subtitle2">Civil Status</Typography>
+                  <Typography variant="subtitle1">{employee.civilStatus || '--'}</Typography>
+                </div>
+                <div className={details}>
+                  <Typography variant="subtitle2">Qualified Dependents</Typography>
+                  <Typography variant="subtitle1">
+                    {employee.numberOfQualifiedDependents}
+                  </Typography>
+                </div>
+                <div className={details}>
+                  <Typography variant="subtitle2">Birth Date</Typography>
+                  <Typography variant="subtitle1">
+                    {(employee.birthDate && moment(employee.birthDate).format('MMM DD, YYYY')) ||
+                      '--'}
+                  </Typography>
+                </div>
+                <div className={details}>
+                  <Typography variant="subtitle2">Nationality</Typography>
+                  <Typography variant="subtitle1">{employee.nationality || '--'}</Typography>
+                </div>
+                <div className={details}>
+                  <Typography variant="subtitle2">Registered Address</Typography>
+                  <Typography variant="subtitle1">{employee.formattedRegisteredAddress}</Typography>
+                </div>
+                <div className={details}>
+                  <Typography variant="subtitle2">Permanent Address</Typography>
+                  <Typography variant="subtitle1">{employee.formattedPermanentAddress}</Typography>
+                </div>
+              </Grid>
+            </Grid>
+            <Grid container spacing={1}>
+              <Grid item xs={12} md={6}>
+                <Typography variant="h5" gutterBottom>
+                  Government IDs
+                </Typography>
+                <div className={details}>
+                  <Typography variant="subtitle2">SSS</Typography>
+                  <Typography variant="subtitle1">{employee.sssNumber || '--'}</Typography>
+                </div>
+                <div className={details}>
+                  <Typography variant="subtitle2">Philhealth</Typography>
+                  <Typography variant="subtitle1">{employee.phicNumber || '--'}</Typography>
+                </div>
+                <div className={details}>
+                  <Typography variant="subtitle2">Pag-Ibig</Typography>
+                  <Typography variant="subtitle1">{employee.hdmfNumber || '--'}</Typography>
+                </div>
+                <div className={details}>
+                  <Typography variant="subtitle2">Tax Identification</Typography>
+                  <Typography variant="subtitle1">{employee.tin || '--'}</Typography>
+                </div>
+              </Grid>
+              <Grid item sx={12} md={6}>
+                <Typography variant="h5" gutterBottom>
+                  Address
+                </Typography>
+                <div className={details}>
+                  <Typography variant="subtitle2">Registered Address</Typography>
+                  <Typography variant="subtitle1">
+                    {employee.formattedRegisteredAddress || '--'}
+                  </Typography>
+                </div>
+                <div className={details}>
+                  <Typography variant="subtitle2">Permanent Address</Typography>
+                  <Typography variant="subtitle1">
+                    {employee.formattedPermanentAddress || '--'}
+                  </Typography>
+                </div>
+              </Grid>
+            </Grid>
+            <Grid container spacing={1}>
+              <Grid item xs={12} md={6}>
+                <div className={details}>
+                  <List>
+                    <Typography variant="h5">Compensation</Typography>
+                    {employee.compensations.map((compensation) => (
+                      <ListItem key={compensation._id}>
+                        <ListItemText
+                          primary="Gross Compensation"
+                          secondary={(
+                            <>
+                              <Typography component="span" variant="body2" color="textPrimary">
+                                {compensation.basicPay}
+                              </Typography>
+                              {` - ${moment(compensation.effectivityDate).format('MMM DD, YYYY')}`}
+                            </>
+                          )}
+                        />
+                      </ListItem>
+                    ))}
+                  </List>
+                </div>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <div className={details}>
+                  <List>
+                    <Typography variant="h5">Statuses</Typography>
+                    {employee.statuses.map((status) => (
+                      <ListItem key={status._id}>
+                        <ListItemText
+                          primary="Status"
+                          secondary={(
+                            <>
+                              <Typography component="span" variant="body2" color="textPrimary">
+                                {status.active ? 'Active' : 'Inactive'}
+                              </Typography>
+                              {` - ${moment(status.effectivityDate).format('MMM DD, YYYY')}`}
+                            </>
+                          )}
+                        />
+                      </ListItem>
+                    ))}
+                  </List>
+                </div>
+              </Grid>
+            </Grid>
+          </>
+        )}
       </Paper>
     </Container>
   );
