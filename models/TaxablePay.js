19e07c5b0810a8c2d39154d2519f8b43cc78ccd0
Darryl Mangibin <darrylmangibin@gmail.com>
2021-02-17 13:48:06 +0800

    Add taxable pays model and update company settings model

---
+++ models/TaxablePay.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const TaxablePaySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please add a taxable'],
+    },
+    company: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Company',
+      required: [true, 'Company must exist'],
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+    autoCreate: true,
+  },
+);
+
+module.exports = mongoose.model('TaxablePay', TaxablePaySchema);
