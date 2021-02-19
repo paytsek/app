c9c13d8f3cc6231defe9fe5de33b535980ca42a6
Darryl Mangibin <darrylmangibin@gmail.com>
2021-02-19 18:10:03 +0800

    Add Employeeform to create an employee

--- client/src/components/Employees/EmployeeForm/index.jsx
+++ client/src/components/Employees/EmployeeForm/index.jsx
@@ -1,17 +1,59 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, Button } from '@material-ui/core';
 
 import BasicInformation from './BasicInformation';
+import PersonalInformation from './PersonalInformation';
+import AddressInformation from './AddressInformation';
+import BasicAdjustment from './BasicAdjustment';
+import BankingInformation from './BankingInformation';
+import GovernmentIds from './GovernmentIds';
+import EmployeeFuntion from './EmployeeFunction';
+import TaxableCompensation from './TaxableCompensation';
+import NonTaxableCompensation from './NonTaxableCompensation';
 
 import useStyles from './styles';
 
 const EmployeeForm = () => {
-  const { gridContainer } = useStyles();
+  const { gridContainer, formButton } = useStyles();
 
   return (
     <Grid container spacing={3} className={gridContainer}>
       <Grid item xs={12}>
         <BasicInformation />
+      </Grid>
+      <Grid item xs={12}>
+        <PersonalInformation />
+      </Grid>
+      <Grid item md={6} xs={12}>
+        <AddressInformation title="Registered Address" />
+      </Grid>
+      <Grid item md={6} xs={12}>
+        <AddressInformation title="Permanent Address" />
+      </Grid>
+      <Grid item xs={12}>
+        <BasicAdjustment />
+      </Grid>
+      <Grid item xs={12}>
+        <BankingInformation />
+      </Grid>
+      <Grid item xs={12}>
+        <GovernmentIds />
+      </Grid>
+      <Grid item xs={12}>
+        <EmployeeFuntion />
+      </Grid>
+      <Grid item md={6} xs={12}>
+        <TaxableCompensation />
+      </Grid>
+      <Grid item md={6} xs={12}>
+        <NonTaxableCompensation />
+      </Grid>
+      <Grid item>
+        <div className={formButton}>
+          <Button color="primary" variant="contained">
+            Save Employee
+          </Button>
+        </div>
       </Grid>
     </Grid>
   );
