24fcd4569faeebb5a2f0aa052db39f673045f2cf
Darryl Mangibin <darrylmangibin@gmail.com>
2021-03-22 16:27:26 +0800

    Remove client side folder and files

--- client/src/axios/index.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const baseURL =
-  (process.env.NODE_ENV === 'development' && 'http://localhost:5000/api/v1') ||
-  (process.env.NODE_ENV === 'production' && 'https://staging-paytsek.herokuapp.com/api/v1');
-
-export default axios.create({
-  baseURL,
-});
--- client/src/pages/Dashboard/index.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Dashboard = () => <div>Admin Dashboard</div>;
-
-export default Dashboard;
