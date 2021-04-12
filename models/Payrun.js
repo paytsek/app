54df99538a4b724afb1f48ddf71bd1843ab3c2cd
Darryl Mangibin <darrylmangibin@gmail.com>
2021-04-12 13:10:27 +0800

    Add payrunItems prop

--- models/Payrun.js
+++ models/Payrun.js
@@ -4,47 +4,60 @@ const PAYRUN_STATUS = ['draft', 'approved', 'void', 'denied'];
 
 const PAYRUN_TAX_PAYMENT = ['full', 'half', 'none'];
 
-const PayrunSchema = new mongoose.Schema({
-  startDate: {
-    type: Date,
-    required: [true, 'Start Date is required'],
+const PayrunSchema = new mongoose.Schema(
+  {
+    startDate: {
+      type: Date,
+      required: [true, 'Start Date is required'],
+    },
+    endDate: {
+      type: Date,
+      required: [true, 'End Date is required'],
+    },
+    status: {
+      type: String,
+      required: [true, 'Status is required'],
+      enum: PAYRUN_STATUS,
+      default: 'draft',
+    },
+    payoutDate: {
+      type: Date,
+      required: [true, 'Payout Date is required'],
+    },
+    totalNetPay: {
+      type: Number,
+      defualt: 0,
+    },
+    special: {
+      type: Boolean,
+      default: false,
+    },
+    displayBeforePayout: {
+      type: Boolean,
+      default: false,
+    },
+    taxPayment: {
+      type: String,
+      enum: PAYRUN_TAX_PAYMENT,
+      default: 'full',
+    },
+    company: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Company',
+      required: [true, 'Must have a company'],
+    },
+    payrunItems: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'PayrunItem',
+      },
+    ],
   },
-  endDate: {
-    type: Date,
-    required: [true, 'End Date is required'],
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   },
-  status: {
-    type: String,
-    required: [true, 'Status is required'],
-    enum: PAYRUN_STATUS,
-    default: 'draft',
-  },
-  payoutDate: {
-    type: Date,
-    required: [true, 'Payout Date is required'],
-  },
-  totalNetPay: {
-    type: Number,
-    defualt: 0,
-  },
-  special: {
-    type: Boolean,
-    default: false,
-  },
-  displayBeforePayout: {
-    type: Boolean,
-    default: false,
-  },
-  taxPayment: {
-    type: String,
-    enum: PAYRUN_TAX_PAYMENT,
-    default: 'full',
-  },
-  company: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Company',
-    required: [true, 'Must have a company'],
-  },
-});
+);
 
 module.exports = mongoose.model('Payrun', PayrunSchema);
