82bff054f6124eeb9cedf44d79ef78e9d5cba7ff
Darryl Mangibin <darrylmangibin@gmail.com>
2021-04-12 11:39:01 +0800

    Add payourDate prop

--- models/Payrun.js
+++ models/Payrun.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+const PAYRUN_STATUS = ['draft', 'approved', 'void', 'denied'];
+
 const PayrunSchema = new mongoose.Schema({
   startDate: {
     type: Date,
@@ -8,6 +10,16 @@ const PayrunSchema = new mongoose.Schema({
   endDate: {
     type: Date,
     required: [true, 'End Date is required'],
+  },
+  status: {
+    type: String,
+    required: [true, 'Status is required'],
+    enum: PAYRUN_STATUS,
+    default: 'draft',
+  },
+  payoutDate: {
+    type: Date,
+    required: [true, 'Payout Date is required'],
   },
 });
 
