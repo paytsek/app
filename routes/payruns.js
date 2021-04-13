92427a4039d61afa417d9066c83aad9ea8c9a8d3
Darryl Mangibin <darrylmangibin@gmail.com>
2021-04-13 11:34:48 +0800

    Add route for updating a payrun

--- routes/payruns.js
+++ routes/payruns.js
@@ -5,7 +5,12 @@ const router = express.Router();
 const administrator = require('../middleware/administrator');
 const auth = require('../middleware/auth');
 const tenant = require('../middleware/tenant');
-const { getPayruns, getPayrun, createPayrun } = require('../controllers/payruns');
+const {
+  getPayruns,
+  getPayrun,
+  createPayrun,
+  updatePayrun,
+} = require('../controllers/payruns');
 
 // api/v1/payruns
 router
@@ -14,6 +19,9 @@ router
   .post(auth, tenant, administrator, createPayrun);
 
 // api/v1/payruns/:id
-router.route('/:id').get(auth, tenant, administrator, getPayrun);
+router
+  .route('/:id')
+  .get(auth, tenant, administrator, getPayrun)
+  .put(auth, tenant, administrator, updatePayrun);
 
 module.exports = router;
