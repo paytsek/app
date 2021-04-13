13b04fb9362192c2d57c26c280492a523f3634d7
Darryl Mangibin <darrylmangibin@gmail.com>
2021-04-13 15:12:47 +0800

    Add integration test for updating payrun's status

--- test/integration/controllers/payruns.test.js
+++ test/integration/controllers/payruns.test.js
@@ -725,3 +725,153 @@ describe('DELETE /api/v1/payruns/:id - deletePayrun', () => {
     });
   });
 });
+
+describe('PUT /api/v1/payruns/:id/status - updatePayrunStatus', () => {
+  describe('Error Response', () => {
+    it('should return error response if not logged in', async () => {
+      const res = await request(app).put(
+        `${url}/${mongoose.Types.ObjectId().toHexString()}/status`,
+      );
+
+      expect(res.status).toBe(401);
+      expect(res.body).toEqual(
+        expect.objectContaining({
+          success: false,
+          errors: expect.objectContaining({ message: 'No token, access denied' }),
+        }),
+      );
+    });
+
+    it('should return error response if no administrator', async () => {
+      const token = await global.signIn();
+      const company = await Company.create({
+        name: 'Full suite',
+        user: mongoose.Types.ObjectId(),
+      });
+
+      const res = await request(app)
+        .put(`${url}/${mongoose.Types.ObjectId().toHexString()}/status`)
+        .set({ 'x-company-tenant': company.slug })
+        .auth(token, { type: 'bearer' });
+
+      expect(res.status).toBe(403);
+      expect(res.body).toEqual(
+        expect.objectContaining({
+          success: false,
+          errors: expect.objectContaining({
+            message: "You don't have permission to access on this route",
+          }),
+        }),
+      );
+    });
+
+    it('should return error response if logged in user is not equal to company user', async () => {
+      const token = await global.signIn();
+      const user = jwt.verify(token, process.env.JWT_SECRET_KEY);
+      const company = await Company.create({
+        name: 'Full suite',
+        user: mongoose.Types.ObjectId(),
+        administrators: [user._id],
+      });
+
+      const res = await request(app)
+        .put(`${url}/${mongoose.Types.ObjectId().toHexString()}/status`)
+        .set({ 'x-company-tenant': company.slug })
+        .auth(token, { type: 'bearer' });
+
+      expect(res.status).toBe(401);
+      expect(res.body).toEqual(
+        expect.objectContaining({
+          success: false,
+          errors: expect.objectContaining({ message: 'Not authorized, access denied' }),
+        }),
+      );
+    });
+
+    it('should return error response if no company set', async () => {
+      const token = await global.signIn();
+
+      const res = await request(app)
+        .put(`${url}/${mongoose.Types.ObjectId().toHexString()}/status`)
+        .auth(token, { type: 'bearer' });
+
+      expect(res.status).toBe(401);
+      expect(res.body).toEqual(
+        expect.objectContaining({
+          success: false,
+          errors: expect.objectContaining({ message: 'No tenant, access denied' }),
+        }),
+      );
+    });
+
+    it('should return error response if values are invalid', async () => {
+      const token = await global.signIn();
+      const user = jwt.verify(token, process.env.JWT_SECRET_KEY);
+      const company = await Company.create({
+        name: 'Full suite',
+        user: user._id,
+        administrators: [user._id],
+      });
+      const payrun = await Payrun.create({
+        startDate: '2021-01-01',
+        endDate: '2021-01-15',
+        company: company._id,
+      });
+
+      const res = await request(app)
+        .put(`${url}/${payrun._id}/status`)
+        .set({ 'x-company-tenant': company.slug })
+        .auth(token, { type: 'bearer' })
+        .send({
+          status: 'Test',
+        });
+
+      expect(res.status).toBe(400);
+      expect(res.body.success).toBeFalsy();
+      expect(res.body).toEqual(
+        expect.objectContaining({
+          errors: expect.objectContaining({
+            status: '`Test` is not a valid enum value for path `status`.',
+          }),
+        }),
+      );
+    });
+  });
+
+  describe('Success Response', () => {
+    it('should retrun success response if entered values are valid', async () => {
+      const token = await global.signIn();
+      const user = jwt.verify(token, process.env.JWT_SECRET_KEY);
+      const company = await Company.create({
+        name: 'Full suite',
+        user: user._id,
+        administrators: [user._id],
+      });
+      const payrun = await Payrun.create({
+        startDate: '2021-01-01',
+        endDate: '2021-01-15',
+        company: company._id,
+        status: 'draft',
+      });
+
+      expect(payrun.status).toMatch(/draft/);
+
+      const res = await request(app)
+        .put(`${url}/${payrun._id}/status`)
+        .set({ 'x-company-tenant': company.slug })
+        .auth(token, { type: 'bearer' })
+        .send({ status: 'approved' });
+
+      expect(res.status).toBe(200);
+      expect(res.body.success).toBeTruthy();
+      expect(res.body).toEqual(
+        expect.objectContaining({
+          payrun: expect.objectContaining({
+            _id: payrun._id.toString(),
+            status: 'approved',
+          }),
+        }),
+      );
+    });
+  });
+});
