fc9d4442e1198ce10fa42246ea84cf5c335c47fc
Darryl Mangibin <darrylmangibin@gmail.com>
2021-04-26 16:57:21 +0800

    Move getCompensation method to have its own file test

--- utils/testUtils.js
+++ utils/testUtils.js
@@ -1,12 +1,22 @@
 const CompanySetting = require('../models/CompanySetting');
 const Company = require('../models/Company');
 const User = require('../models/User');
+const Employee = require('../models/Employee');
+const Compensation = require('../models/Compensation');
 
 class TestUtil {
   static responseSetObject(slug = '') {
     return {
       'x-company-tenant': slug,
     };
+  }
+
+  static createCompensation(compensation) {
+    return Compensation.create(compensation);
+  }
+
+  static createEmployee(employee) {
+    return Employee.create(employee);
   }
 
   static createCompanySetting(companySetting) {
